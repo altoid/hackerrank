1db67f79c1f868a46b8952d7e2142eac21d5760b
Doug Tomm <dtomm@radiumone.com>
2018-08-03 10:11:03 -0700

    read input from file

--- min_swaps_2/solution.py
+++ min_swaps_2/solution.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
 import random
+import fileinput
 
-LIMIT=10
+LIMIT = 10
 
-if __name__ == '__main__':
-    arr = random.sample(xrange(1, LIMIT + 1), LIMIT)
-    print arr
-    checks = [x for x in [0] * LIMIT]
-    print checks
+
+def minimumSwaps(arr):
+    # print arr
+    checks = [x for x in [0] * len(arr)]
+    # print checks
     total_swaps = 0
 
     zeroes = filter(lambda x: x[1] == 0, enumerate(checks))
@@ -20,7 +21,7 @@ if __name__ == '__main__':
         pass
 
     while t:
-        print "========== orbit"
+        # print "========== orbit"
         orbit_len = -1
         pos = t[0]
         while checks[pos] == 0:
@@ -28,13 +29,13 @@ if __name__ == '__main__':
             orbit_len += 1
             e = arr[pos]
             if e == pos + 1:
-                print "arr[%s] == %s, nothing to do" % (pos, e)
+                # print "arr[%s] == %s, nothing to do" % (pos, e)
                 break
 
-            print "arr[%s] is %s but belongs at position %s" % (pos, e, e - 1)
+            # print "arr[%s] is %s but belongs at position %s" % (pos, e, e - 1)
             pos = e - 1
 
-        print checks, "orbit_len = %s" % orbit_len
+        # print checks, "orbit_len = %s" % orbit_len
         total_swaps += orbit_len
 
         zeroes = filter(lambda x: x[1] == 0, enumerate(checks))
@@ -45,4 +46,12 @@ if __name__ == '__main__':
         except StopIteration as no_mo:
             pass
 
-    print "total_swaps = %s" % total_swaps
+    return total_swaps
+
+
+if __name__ == '__main__':
+    fi = fileinput.FileInput()
+    nelements = map(int, fi.readline().strip().split(' '))
+    elements = map(int, fi.readline().strip().split(' '))
+
+    print minimumSwaps(elements)
