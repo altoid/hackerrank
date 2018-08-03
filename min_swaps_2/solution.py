cce7d292af60f8ec5183c4106db367e48ad3a755
Doug Tomm <dtomm@radiumone.com>
2018-08-03 09:47:46 -0700

    compute swaps

--- min_swaps_2/solution.py
+++ min_swaps_2/solution.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     print arr
     checks = [x for x in [0] * LIMIT]
     print checks
+    total_swaps = 0
 
     zeroes = filter(lambda x: x[1] == 0, enumerate(checks))
     zeroes_itr = iter(zeroes)
@@ -18,10 +19,13 @@ if __name__ == '__main__':
     except StopIteration as no_mo:
         pass
 
-    if t:
+    while t:
+        print "========== orbit"
+        orbit_len = -1
         pos = t[0]
         while checks[pos] == 0:
             checks[pos] = 1
+            orbit_len += 1
             e = arr[pos]
             if e == pos + 1:
                 print "arr[%s] == %s, nothing to do" % (pos, e)
@@ -29,5 +33,16 @@ if __name__ == '__main__':
 
             print "arr[%s] is %s but belongs at position %s" % (pos, e, e - 1)
             pos = e - 1
-        
-    print checks
+
+        print checks, "orbit_len = %s" % orbit_len
+        total_swaps += orbit_len
+
+        zeroes = filter(lambda x: x[1] == 0, enumerate(checks))
+        zeroes_itr = iter(zeroes)
+        t = None
+        try:
+            t = zeroes_itr.next()
+        except StopIteration as no_mo:
+            pass
+
+    print "total_swaps = %s" % total_swaps
