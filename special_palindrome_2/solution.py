2e801907207211bdcb69500269659dc2c58aa273
Doug Tomm <dtomm@radiumone.com>
2018-08-04 08:00:43 -0700

    submitted

--- special_palindrome_2/solution.py
+++ special_palindrome_2/solution.py
@@ -18,12 +18,38 @@ def rfunc(accum_value, c):
     return accum_value
 
 
-def substrCount(str):
+def substrCount(unused, str):
     chars = list(str)
-    print chars
-    result = reduce(rfunc, chars, [])
-    print result
-    return 0
+    # print str, len(str)
+    # print chars
+    charcounts = reduce(rfunc, chars, [])
+
+    # traverse the result.
+    # if any char count is > 1, add n(n + 1)/2 to result
+    # if any char count is 1, check preceding and following char.  if they are the same, add 1.
+    # print charcounts
+
+    result = 0
+    n = charcounts[0][1]
+    result += n * (n + 1) / 2
+    if len(charcounts) > 1:
+        n = charcounts[-1][1]
+        result += n * (n + 1) / 2
+
+    # do it this way so we don't have to do a bounds check on every iteration
+    for i in xrange(1, len(charcounts) - 1):
+        # print charcounts[i]
+        n = charcounts[i][1]
+        result += n * (n + 1) / 2
+        if n == 1:
+            pred = charcounts[i - 1][0]
+            succ = charcounts[i + 1][0]
+            if pred == succ:
+                predcount = charcounts[i - 1][1]
+                succcount = charcounts[i + 1][1]
+                result += min(predcount, succcount)
+
+    return result
 
 
 if __name__ == '__main__':
@@ -31,4 +57,5 @@ if __name__ == '__main__':
     nchars = fi.readline().strip()
     str = fi.readline().strip()
 
-    result = substrCount(str)
+    result = substrCount(nchars, str)
+    print "result = %s" % result
