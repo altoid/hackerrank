4ecb406a7f01e114982073f56fcdde630a73e072
Doug Tomm <dtomm@radiumone.com>
2018-08-05 08:11:44 -0700

    submitted

--- sql/new_companies.sql
+++ sql/new_companies.sql
@@ -1,87 +1,54 @@
 use hackerrank;
 
 select
-distinct
 c.*,
-lm.lead_manager_code,
-sm.senior_manager_code,
-m.manager_code,
-e.employee_code
-from company c
-inner join lead_manager lm on lm.company_code = c.company_code
-inner join senior_manager sm on sm.company_code = lm.company_code
-inner join manager m on m.company_code = sm.company_code
-inner join employee e on e.company_code = m.company_code
-;
-
-select
-distinct
-c.*,
-lm.lead_manager_code
-from company c
-inner join lead_manager lm on lm.company_code = c.company_code
-;
-
-select
-distinct
-c.*,
-sm.senior_manager_code
-from company c
-inner join senior_manager sm on sm.company_code = c.company_code
-;
-
-select
-distinct
-c.*,
-m.manager_code
-from company c
-inner join manager m on m.company_code = c.company_code
-;
-
-select
-distinct
-c.*,
-e.employee_code
-from company c
-inner join employee e on e.company_code = c.company_code
-;
-
-select company_code, count(*) c, 'lmcounts' type from
+t1.lmcounts,
+t2.smcounts,
+t3.mcounts,
+t4.ecounts
+from
+company c
+inner join
+(
+select company_code, count(*) lmcounts from
 (
 select distinct
 company_code, lead_manager_code
  from lead_manager
 ) lmcounts
 group by company_code
-
-union all
-
-select company_code, count(*) c, 'smcounts' type from
+) t1 on t1.company_code = c.company_code
+inner join
+(
+select company_code, count(*) smcounts from
 (
 select distinct
 company_code, senior_manager_code
  from senior_manager
 ) smcounts
 group by company_code
-
-union all
-
-select company_code, count(*) c, 'mcounts' type from
+) t2 on t1.company_code = t2.company_code
+inner join
+(
+select company_code, count(*) mcounts from
 (
 select distinct
 company_code, manager_code
  from manager
 ) mcounts
 group by company_code
-
-union all
-
-select company_code, count(*) c, 'ecounts' from
+) t3 on t2.company_code = t3.company_code
+inner join
+(
+select company_code, count(*) ecounts from
 (
 select distinct
 company_code, employee_code
  from employee
 ) ecounts
 group by company_code
+) t4 on t3.company_code = t4.company_code
+
+order by c.company_code
 ;
 
