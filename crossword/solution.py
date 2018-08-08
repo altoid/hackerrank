b8fbbed9bbc9e977e6afbdd105ff55d5d2e8448d
Doug Tomm <dtomm@radiumone.com>
2018-08-08 07:39:28 -0700

    combine code into 1 file

--- crossword/solution.py
+++ crossword/solution.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import fileinput
-import permute
 
 BOARDSIZE = 10
 ACROSS = 0
@@ -25,6 +24,55 @@ class Run(object):
 
     def __len__(self):
         return self.length
+
+
+def next_permutation(mylist, cmp_function):
+    # return the permutation that lexicographically follows
+    # <mylist>, or None if there is no such permutation
+    #
+    # algorithm:
+    # - scan RL to first item x that is smaller than
+    #   the one to its right
+    # - swap it with the smallest item to the right
+    #   of x that is larger than x (y)
+    # - sort the list [x+1:]
+
+    l = len(mylist)
+    if l < 2:
+        return
+
+    current = l - 2
+    following = l - 1
+    while current >= 0:
+        if cmp_function(mylist[current], mylist[following]) < 0:
+            #print "ack - current = %s, following = %s, list = %s" % (current, following, mylist)
+            # find the smallest item to the right of
+            # mylist[current] that is bigger
+            # than mylist[current]
+            k = following
+            m = mylist[k]
+            mi = k
+            while k < l:
+                # mylist[current] < mylist[k] < m:
+                if cmp_function(mylist[current], mylist[k]) < 0 and cmp_function(mylist[k], m) < 0:
+                    m = mylist[k]
+                    mi = k
+                k += 1
+
+            mylist_copy = list(mylist)
+            mylist_copy[current], mylist_copy[mi] = mylist_copy[mi], mylist_copy[current]
+            return mylist_copy[:following] + sorted(mylist_copy[following:])
+        
+        current -= 1
+        following -= 1
+    
+
+def permute(mylist, cmp_function=cmp):
+    x = sorted(mylist, cmp_function)
+
+    while x is not None:
+        yield x
+        x = next_permutation(x, cmp_function)
 
 
 # words are sorted by length, then alphabetically
@@ -80,9 +128,6 @@ def clear_board():
 
 def test_solution(soln):
     for i in xrange(len(all_runs)):
-        print all_runs[i]
-        print soln[i]
-
         if not word_fits(soln[i], all_runs[i]):
             return False
 
@@ -193,7 +238,7 @@ if __name__ == '__main__':
     s = sorted(words, cmp=my_compare)
     print s
     print "#" * 44
-    for x in permute.permute(s, my_compare):
+    for x in permute(s, my_compare):
         lens = map(len, x)
         if lens == all_lengths:
             if test_solution(x):
