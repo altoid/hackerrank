a9bc47f7733a943fa73ac6c71ade03fdaf172554
Doug Tomm <dtomm@radiumone.com>
2018-08-08 07:15:24 -0700

    implement run as a class

--- crossword/solution.py
+++ crossword/solution.py
@@ -4,8 +4,24 @@ import fileinput
 import permute
 
 BOARDSIZE = 10
+ACROSS = 0
+DOWN = 1
 
 # assume no 1-letter words
+
+class Run(object):
+    def __init__(self, r, c, length, dir):
+        self.row = r
+        self.col = c
+        self.length = length
+        self.dir = dir
+
+    def __str__(self):
+        return "[row %s, col %s, len %s, dir %s]" % (
+            self.row, self.col, self.length, 'DOWN' if self.dir else 'ACROSS')
+
+    def __repr__(self):
+        return str(self)
 
 # words are sorted by length, then alphabetically
 def my_compare(a, b):
@@ -16,6 +32,9 @@ def my_compare(a, b):
         return -1
 
     return 1
+
+def test_solution(soln):
+    print soln
 
 if __name__ == '__main__':
     fi = fileinput.FileInput()
@@ -46,25 +65,25 @@ if __name__ == '__main__':
                 if c == '+':
                     pass
                 elif c == '-':
-                    currentrun = (row, col, 1)
+                    currentrun = Run(row, col, 1, ACROSS)
 
             elif lastchar == '-':
                 if c == '+':
-                    if currentrun[2] > 1:
+                    if currentrun.length > 1:
                         across_runs.append(currentrun)
                     currentrun = None
                 elif c == '-':
-                    currentrun = (currentrun[0], currentrun[1], currentrun[2] + 1)
+                    currentrun.length += 1
 
             elif lastchar == '+':
                 if c == '+':
                     pass
                 elif c == '-':
-                    currentrun = (row, col, 1)
+                    currentrun = Run(row, col, 1, ACROSS)
 
             lastchar = c
             
-        if currentrun and currentrun[2] > 1:
+        if currentrun and currentrun.length > 1:
             across_runs.append(currentrun)
             
     # scan the board: down
@@ -85,32 +104,32 @@ if __name__ == '__main__':
                 if c == '+':
                     pass
                 elif c == '-':
-                    currentrun = (row, col, 1)
+                    currentrun = Run(row, col, 1, DOWN)
 
             elif lastchar == '-':
                 if c == '+':
-                    if currentrun[2] > 1:
+                    if currentrun.length > 1:
                         down_runs.append(currentrun)
                     currentrun = None
                 elif c == '-':
-                    currentrun = (currentrun[0], currentrun[1], currentrun[2] + 1)
+                    currentrun.length += 1
 
             elif lastchar == '+':
                 if c == '+':
                     pass
                 elif c == '-':
-                    currentrun = (row, col, 1)
+                    currentrun = Run(row, col, 1, DOWN)
 
             lastchar = c
             
-        if currentrun and currentrun[2] > 1:
+        if currentrun and currentrun.length > 1:
             down_runs.append(currentrun)
 
     print "across_runs: %s" % across_runs
     print "down_runs: %s" % down_runs
 
     all_runs = across_runs + down_runs
-    all_lengths = map(lambda x: x[2], all_runs)
+    all_lengths = map(lambda x: x.length, all_runs)
     print "all_runs: %s" % all_runs
     print "all_lengths: %s" % all_lengths
 
@@ -123,6 +142,6 @@ if __name__ == '__main__':
     for x in permute.permute(s, my_compare):
         lens = map(len, x)
         if lens == all_lengths:
-            print x
+            test_solution(x)
 
 
