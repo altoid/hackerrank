5f33eaa0552767d8b281da93bc03180a1daa1702
Doug Tomm <dtomm@radiumone.com>
2018-09-03 20:46:42 -0700

    complete

--- almost_sorted/solution.py
+++ almost_sorted/solution.py
@@ -2,10 +2,50 @@
 
 import fileinput
 
+
+def almostSorted(arr):
+    left = None
+    right = None
+    interval = None
+    for i in xrange(len(arr) - 1):
+        if arr[i] < arr[i + 1]:
+            if left:
+                right = i
+                interval = (left, right)
+        else:
+            if interval:
+                print 'no'
+                return
+
+            if not left:
+                left = i
+
+    # we have at most one interval
+    if not interval:
+        print 'yes'
+        return
+
+    # check if we are sorted after swap/reverse
+    left, right = interval
+    if left > 0:
+        if arr[left - 1] > arr[right]:
+            print 'no'
+            return
+
+    if right < len(arr) - 1:
+        if arr[right + 1] < arr[left]:
+            print 'no'
+            return
+
+    print 'yes'
+    operation = 'reverse' if right - left > 1 else 'swap'
+    print '%s %s %s' % (operation, left + 1, right + 1)
+
+
 if __name__ == '__main__':
     fi = fileinput.FileInput()
 
     nelements = fi.readline()
     elements = map(int, fi.readline().split(' '))
 
-    print elements
+    almostSorted(elements)
