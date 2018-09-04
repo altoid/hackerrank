4c6cd75b857be9c0c2eeacf58b9e33f3c1f6b11d
Doug Tomm <dtomm@radiumone.com>
2018-09-04 08:26:50 -0700

    added check_swap

--- almost_sorted/solution.py
+++ almost_sorted/solution.py
@@ -3,6 +3,68 @@
 import fileinput
 
 
+def check_swap(arr):
+    # len(arr) > 2
+    toobig = []
+    toosmall = []
+    if arr[0] > arr[1]:
+        toobig.append(0)
+    if arr[-2] > arr[-1]:
+        toosmall.append(len(arr) - 1)
+
+    for i in xrange(1, len(arr) - 1):
+        l = arr[i - 1]
+        m = arr[i]
+        r = arr[i + 1]
+        if l > r:
+            return
+        if m < l:
+            toosmall.append(i)
+            continue
+
+        if m > r:
+            toobig.append(i)
+            continue
+
+    if len(toobig) != 1:
+        return
+
+    if len(toosmall) != 1:
+        return
+
+    # see if swapping preserves order
+    left = toobig[0]
+    right = toosmall[0]
+    leftisgood = False
+    rightisgood = False
+    if left > 0:
+        l = arr[left - 1]
+        m = arr[right]
+        r = arr[left + 1]
+        if l < m < r:
+            leftisgood = True
+    else:
+        m = arr[right]
+        r = arr[left + 1]
+        if m < r:
+            leftisgood = True
+
+    if right < len(arr) - 1:
+        l = arr[right - 1]
+        m = arr[left]
+        r = arr[right + 1]
+        if l < m < r:
+            rightisgood = True
+    else:
+        l = arr[right - 1]
+        m = arr[left]
+        if m < r:
+            rightisgood = True
+
+    if leftisgood and rightisgood:
+        return "swap %s and %s" % (left, right)
+
+
 def almostSorted(arr):
     left = None
     right = None
