3d23b313d8efebe7047b0f49d00a808e4f929c6a
Doug Tomm <dtomm@radiumone.com>
2018-09-27 08:00:20 -0700

    submitted

--- median/solution.py
+++ median/solution.py
@@ -5,49 +5,6 @@ from pprint import pprint, pformat
 import unittest
 
 
-def locate_helper(arr, a, b, e):
-    if b >= e:
-        return b
-
-    # find the index of the smallest element in arr that is > a.
-    # just find the index of a and scan rightward until we find
-    # what we want.
-
-    m = (b + e) / 2
-
-    if a < arr[m]:  # arr[b] <= a <= arr[m - 1]
-        e = m - 1
-    else:  # arr[m] <= a <= arr[e]
-        b = m + 1
-    return locate_helper(arr, a, b, e)
-
-
-def locate(arr, a):
-    """
-
-    :param arr:
-    :param a:
-    :return:  the index of the smallest element in arr that is > a.  if there is nothing larger than a in arr,
-    return -1.
-    """
-
-    # for i in xrange(len(arr)):
-    #     if arr[i] > a:
-    #         return i
-    # return -1
-
-    if not arr:
-        return -1
-
-    if arr[-1] <= a:
-        return -1
-
-    split = locate_helper(arr, a, 0, len(arr) - 1)
-    while arr[split] <= a:
-        split += 1
-    return split
-
-
 def findMedian(array):
     """
     find the median of an unsorted array.  the number of elements in the array is guaranteed to be
@@ -57,19 +14,8 @@ def findMedian(array):
     :return:
     """
 
-    if len(array) == 1:
-        return array[0]
-
-    scratch = []
-
-    for a in array:
-        split = locate(scratch, a)
-        if split == -1:
-            scratch.append(a)
-        else:
-            scratch = scratch[:split] + [a] + scratch[split:]
-
-    return scratch[len(scratch) / 2]
+    arr = sorted(array)
+    return arr[len(arr) / 2]
 
 
 class Tests(unittest.TestCase):
