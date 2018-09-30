fecb6f3dad8430c0072bcfafd06b19c2492ec0b4
Doug Tomm <dtomm@radiumone.com>
2018-09-30 07:58:48 -0700

    checkpoint

--- twins/solution.py
+++ twins/solution.py
@@ -2,14 +2,15 @@
 
 import math
 
-SIZE = 10000
+SIZE = 100
 
-arr = [x for x in xrange(SIZE)]
+LARGE = 10 ** 2
+SEGMENT_SIZE = 10 ** 1
+
 
 def sieve(arr):
     arr[0] = 0
     arr[1] = 0
-    primes = []
 
     # find index of next prime in arr
     
@@ -19,14 +20,12 @@ def sieve(arr):
         while arr[i] == 0:
             i += 1
         
-        primes.append(i)
-        
         for j in xrange(i * 2, SIZE, i):
             arr[j] = 0
     
         i += 1
 
-    print primes
+    return filter(lambda x: x != 0, arr)
 
 #sieve(arr)
 #print filter(lambda x: x != 0, arr)
@@ -40,14 +39,75 @@ def sieve(arr):
 # for this problem, stop at the first segment whose right hand endpoint
 # is bigger than m.
 
-LARGE = 10 ** 3
-SEGMENT_SIZE = 10 ** 2
 
-def segmented_sieve(arr, primes_so_far, left, right):
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+
+    return a
+
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
+
+
+def roundup(a, b):
+    """
+    round up a to the nearest multiple of b
+    """
+    return ((a + b - 1) / b) * b
+
+
+def segmented_sieve(primes_so_far, left, right):
     arr = [x for x in xrange(left, right)]
-    print arr[0], arr[SEGMENT_SIZE - 1]
+
+    # zero out 1
+    if left == 0:
+        arr[1] = 0
+
+    print arr
+
+    # sieve out elements using primes collected from previous iterations
+    for p in primes_so_far:
+        if p * 2 > right:
+            break
+
+        # smallest multiple of p that is >= than max(left, p * 2)
+        m = max(left, p * 2)
+        r = roundup(m, p)
+
+        for j in xrange(r, right, p):
+            arr[j - left] = 0
+
+    # now sieve out primes from arr using prime factors we don't have yet
+    
+    # find index of next prime in arr
+    i = 0
+    
+    while i < SEGMENT_SIZE and arr[i] < math.sqrt(right):
+        while i < SEGMENT_SIZE and arr[i] == 0:
+            i += 1
+
+        if i >= SEGMENT_SIZE or arr[i] < math.sqrt(right):
+            break
+
+        for j in xrange(arr[i] * 2, right, arr[i]):
+            arr[j - left] = 0
+
+        i += 1
+
+    # new batch of primes
+    primes_so_far += filter(lambda x: x != 0, arr)
+
 
 i = 0
+collected_primes = []
 while i < LARGE:
-    segmented_sieve([], [], i, i + SEGMENT_SIZE)
+    segmented_sieve(collected_primes, i, i + SEGMENT_SIZE)
     i += SEGMENT_SIZE
+
+print "segmented_sieve: ", collected_primes
+
+primes = sieve([x for x in xrange(SIZE)])
+
+print "regular sieve: ", primes
