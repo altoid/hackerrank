c21b774b199a4c6a762425f5d5f419fc11cda73b
Doug Tomm <dtomm@radiumone.com>
2018-09-30 09:44:51 -0700

    handle boundary conditions - where b is prime

--- twins/solution.py
+++ twins/solution.py
@@ -2,9 +2,11 @@
 
 import math
 import unittest
+import fileinput
 
 
-def sieve(arr):
+def simple_sieve(up_to):
+    arr = [x for x in xrange(up_to + 1)]
     arr[0] = 0
     arr[1] = 0
 
@@ -23,13 +25,13 @@ def sieve(arr):
 
     return filter(lambda x: x != 0, arr)
 
-#sieve(arr)
+#simple_sieve(arr)
 #print filter(lambda x: x != 0, arr)
 
-# segmented sieve:  for n large (e.g. 10 ** 9)
+# segmented simple_sieve:  for n large (e.g. 10 ** 9)
 # work on subarrays of size at most 10 ** 7
 # collect the primes from the first segment
-# use those to sieve out primes in the next segment
+# use those to simple_sieve out primes in the next segment
 # etc.
 #
 # for this problem, stop at the first segment whose right hand endpoint
@@ -43,66 +45,66 @@ def roundup(a, b):
     return ((a + b - 1) / b) * b
 
 
-def segmented_sieve_helper(primes_so_far, left, right, segment_size):
-    arr = [x for x in xrange(left, right)]
+def segmented_sieve(a, b):
+    # gimme all primes in the interval [a, b]
 
-    # zero out 1
-    if left == 0:
-        arr[1] = 0
+    # keep caller from being a dick
+    a, b = min(a, b), max(a, b)
 
-#    print arr
+    if a < 0 or b < 0:
+        raise Exception("don't be a dick")
 
-    # sieve out elements using primes collected from previous iterations
-    for p in primes_so_far:
-        if p * 2 > right:
+    # get all the primes up to sqrt(size) using simple sieve
+    squirt = int(math.sqrt(b))
+    primes = simple_sieve(squirt)
+
+    upto = b + 1  # want ranges to include b
+    arr = [x for x in xrange(a, upto)]
+
+    for p in primes:
+        if p * 2 > b:
             break
 
-        # smallest multiple of p that is >= than max(left, p * 2)
-        m = max(left, p * 2)
+        # smallest multiple of p that is >= than max(a, p * 2)
+        m = max(a, p * 2)
         r = roundup(m, p)
 
-        for j in xrange(r, right, p):
-            arr[j - left] = 0
+        for j in xrange(r, upto, p):
+            arr[j - a] = 0
 
-    # now sieve out primes from arr using prime factors we don't have yet
-    
-    # find index of next prime in arr
-    i = 0
-    
-    while i < segment_size and arr[i] < math.sqrt(right):
-        while i < segment_size and arr[i] == 0:
-            i += 1
-
-        if i >= segment_size or arr[i] >= math.sqrt(right):
-            break
-
-        for j in xrange(arr[i] * 2, right, arr[i]):
-            arr[j - left] = 0
-
-        i += 1
-
-    # new batch of primes
-    primes_so_far += filter(lambda x: x != 0, arr)
+    result = filter(lambda x: x > 1, arr)
+    return result
 
 
-def segmented_sieve(size, segment_size):
-    i = 0
-    collected_primes = []
-    while i < size:
-        segmented_sieve_helper(collected_primes, i, i + segment_size, segment_size)
-        i += segment_size
-    return collected_primes
+if __name__ == '__main__':
+    fi = fileinput.FileInput()
 
 
 class Tests(unittest.TestCase):
     def test1(self):
-        size = 100
-        segment_size = 10
+        size = 101
 
-        old_way = sieve([x for x in xrange(size)])
-        new_way = segmented_sieve(size, segment_size)
-
-        print old_way
-        print new_way
+        old_way = simple_sieve(size)
+        new_way = segmented_sieve(0, size)
 
         self.assertEqual(old_way, new_way)
+
+    def test2(self):
+        size = 10000
+
+        old_way = simple_sieve(size)
+        new_way = segmented_sieve(0, size)
+
+        self.maxDiff = None
+        self.assertEqual(old_way, new_way)
+
+    # def test3(self):
+    #     size = 10 ** 9
+    #     primes = segmented_sieve(size - 10 ** 6, size)
+    #     print len(primes)
+
+    def test4(self):
+        primes = segmented_sieve(0, 13)
+        print primes
+        self.assertEqual([2, 3, 5, 7, 11, 13], primes)
+
