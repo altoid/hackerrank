d79465ae42afc3bf6452cb2f7ed51fb82b4d765b
Doug Tomm <dtomm@radiumone.com>
2018-10-25 08:00:54 -0700

    comment out printlns

--- scala/src/main/scala/chandrima_xor.scala
+++ scala/src/main/scala/chandrima_xor.scala
@@ -3,14 +3,35 @@ import scala.collection.mutable.ArrayBuffer
 // max value for signed long is 9223372036854775807, which is > 10 ** 18.
 // so 10 ** 18 fits in a long
 
-object chandrima_xor {
+// successor algorithm:
+// find the number we have to add to n to result in a number that is kosher.
+//
+// need to find the next larger number that does not have consecutive 1 bits.
+//
+// to do this we need to add to the original number a value that causes its
+// 1111 substrings to roll over to 10000:
+//
+// 000000000000000010010001100001111110011100101010000000000000
+//                        ^^    ^^^^^^  ^^^
+//                         1     11111   11
+//
+// we'll take n, shift it 1 to the left, then AND this with n
+//
+// 000000000000000010010001100001111110011100101010000000000000
+// 000000000000000100100011000011111100111001010100000000000000   << shifted 1 to the left
+// ------------------------------------------------------------
+//                        1     11111   11                       << a
+//                         1     11111   11                      << b
+// take the & and then shift 1 position to right.  then do !a & b.
+// this is what we add to the original number.  if the result still has
+// consecutive 11 bits, do it again.
 
-  // return true iff n has no runs of consecutive 1 bits.
-  def isKosher(n: Long): Boolean = (n & (n << 1)) == 0
+
+object chandrima_xor {
 
   def bitsToLong(bitstring: String): Long = (bitstring.toList.map(_.asDigit) foldLeft 0L)((b, a) => 2 * b + a)
 
-  def longToBits(n: Long, fillTo: Int = 0): String = {
+  def longToBits(n: Long, fillTo: Int = 60): String = {
     // left pad with 0 until the string is fillTo long.  if fillTo is 0, do not pad.
     val bits = n.toBinaryString
 
@@ -18,37 +39,10 @@ object chandrima_xor {
     Array.fill(padding)(0).mkString ++ bits
   }
 
-  def successor(n: Long): Long = {
-    // algorithm:
-    // find the number we have to add to n to result in a number that is kosher.  if we cannot find such a number,
-    // throw exception.
-    //
-    // need to find the next larger number that does not have consecutive 1 bits.
-    //
-    // to do this we need to add to the original number a value that causes its
-    // 1111 substrings to roll over to 10000:
-    //
-    // 000000000000000010010001100001111110011100101010000000000000
-    //                        ^^    ^^^^^^  ^^^
-    //
-    //                         1     11111   11
-    //
-    // we'll take n, shift it 1 to the left, then AND this with n
-    //
-    // 000000000000000010010001100001111110011100101010000000000000
-    // 000000000000000100100011000011111100111001010100000000000000   << shifted 1 to the left
-    // ------------------------------------------------------------
-    //                        1     11111   11                       << a
-    //                         1     11111   11                      << b
-    //  a b  f
-    //  1 0  0
-    //  1 1  0
-    //  0 1  1
-    //  0 0  0
-    //take the & and then shift 1 position to right.  then do !a & b.
-    //this is what we add to the original number.  if the result still has
-    //consecutive 11 bits, do it again.
+  // return true iff n has no runs of consecutive 1 bits.
+  def isKosher(n: Long): Boolean = (n & (n << 1)) == 0
 
+  def successor(n: Long): Long = {
     def addend(n: Long): Long = {
       if (isKosher(n)) 1
       else {
@@ -60,10 +54,10 @@ object chandrima_xor {
       }
     }
 
-    println("------------------")
+//    println("------------------")
     var a = addend(n)
     var result = n + a
-    println(longToBits(n, 60) + " << n")
+//    println(longToBits(n) + " << n")
     while (!isKosher(result)) {
       a = addend(result)
 //      println(longToBits(a, 60) + " << a")
@@ -72,14 +66,14 @@ object chandrima_xor {
       result += a
     }
 //    println(longToBits(a, 60) + " << a")
-    println(longToBits(result, 60) + " << result")
+//    println(longToBits(result) + " << result")
     result
   }
 
   def solve(arr: Array[Long]): Long = {
     var filtered = arr.filter(isKosher(_))
 
-    println(arr.mkString(" "))
+//    println(arr.mkString(" "))
 
     var nremoved = arr.length - filtered.length
     var last = filtered.last
@@ -88,7 +82,7 @@ object chandrima_xor {
       filtered = filtered :+ last
       nremoved -= 1
     }
-    println(filtered.mkString(" "))
+//    println(filtered.mkString(" "))
     (filtered foldLeft 0L)((b, a) => a ^ b)
   }
 
@@ -112,7 +106,7 @@ object chandrima_xor {
 //    println(bitsToLong(morebits).toBinaryString.toArray.map(_.asDigit).mkString)
 
 //    n = 1
-//    println(longToBits(n, 60))
+//    println(longToBits(n))
 //    while (n < 256) {
 //      n = successor(n)
 //    }
