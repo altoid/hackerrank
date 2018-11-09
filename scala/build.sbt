f09b70706330b3fad4e97b036e6cb2fdf6d44a34
Doug Tomm <dtomm@radiumone.com>
2018-11-09 08:08:12 -0800

    checkpoint

--- scala/build.sbt
+++ scala/build.sbt
@@ -1,4 +1,4 @@
-name := "hackerrank"
+name := "scala"
 
 version := "0.1"
 
--- scala/src/main/scala/CamelCase.scala
+++ scala/src/main/scala/CamelCase.scala
@@ -3,13 +3,14 @@ object CamelCase {
   def camelcase(s: String): Int = {
     val uppers = s.filter(_.isUpper)
     println(uppers)
-
-    0
+    uppers.length + 1
   }
 
   def main(args: Array[String]): Unit = {
-    camelcase("")
-    camelcase("aoeu")
-    camelcase("aAaAaAa")
+    val tests = List("", "aoeu", "aBcDeFgHi")
+
+    for (t <- tests) {
+      println(camelcase(t))
+    }
   }
 }
