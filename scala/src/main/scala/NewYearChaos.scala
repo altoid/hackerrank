e793958cf2e95ce2d22cf631c2315c6e064c7816
Doug Tomm <dtomm@radiumone.com>
2018-12-08 00:14:19 -0800

    submitted

--- scala/src/main/scala/NewYearChaos.scala
+++ scala/src/main/scala/NewYearChaos.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 /*
 traverse the list, looking at each item that is bigger than its array index.  if any of these differences
 is greater than two, 'too chaotic.'  otherwise, min bribes is the sum of these differences.
@@ -5,57 +7,41 @@ is greater than two, 'too chaotic.'  otherwise, min bribes is the sum of these d
  */
 object NewYearChaos {
   def minimumBribes(q: Array[Int]): Unit = {
-    println("============================== " + q.mkString(" "))
+    // we can do this in linear time.  we march through the array, keeping track of all of the possible legal values
+    // that may be in each slot.  since no member of the queue may advance more than 2 spaces forward, there are
+    // 3 possible legal values for each position in the queue.
 
-    // add 1 to each index
-    var q_with_indices = q.zipWithIndex.map(t => (t._1, t._2 + 1))
-    println(q_with_indices.mkString(" "))
-    val q_with_indices_map = q_with_indices.toMap
-    val displacements = q_with_indices.map(t => (t._1, (t._1 - t._2)))
-    println(displacements.mkString(" "))
+    val legalValues = mutable.ArrayBuffer[Int](1, 2, 3)
+    var totalBribes = 0
+    var abort: Boolean = false
+    var i = 0
 
-    var upto: Option[Int] = None
-
-    for (i <- 0 until displacements.length - 1) {
-      if (displacements(i)._2 > displacements(i + 1)._2) {
-        upto = Some(i + 1)
+    while (!abort && i < q.size) {
+      if (q(i) == legalValues(0)) {
+        legalValues(0) = legalValues(1)
+        legalValues(1) = legalValues(2)
+        legalValues(2) += 1
       }
+      else if (q(i) == legalValues(1)) {
+        // we haven't seen legalValues(0) yet, so don't change it
+        legalValues(1) = legalValues(2)
+        legalValues(2) += 1
+        totalBribes += 1
+      }
+      else if (q(i) == legalValues(2)) {
+        // we haven't seen legalValues(0) or (1) yet, so don't change them
+        legalValues(2) += 1
+        totalBribes += 2
+      }
+      else {
+        println("Too chaotic")
+        abort = true
+      }
+      i += 1
     }
-
-    // we don't need to look at every number.  we can stop looking when we know the rest of the array is sorted.
-    // this is true when the displacements do not decrease.  so we look for a nondecreasing subarray of displacements
-    // that continues to the end.
-
-    upto match {
-      case None => {
-        println("the whole list is sorted")
-        q_with_indices = Array[(Int, Int)]()
-      }
-      case Some(p) => {
-        println(s"upto: ${p}")
-        q_with_indices = q_with_indices.take(p)
-      }
+    if (!abort) {
+      println(s"$totalBribes")
     }
-
-    println(q_with_indices.mkString(" "))
-    // for each number, get its index.  then count the number of smaller numbers
-    // with larger indices.
-    val chaotic = displacements.find(_._2 > 2)
-    chaotic match {
-      case Some(_) => println("Too chaotic")
-      case None => {
-        val e = for (d <- q_with_indices) yield {
-          // println(s"number ${d._1}, displacement ${d._2}")
-          val filtered = q_with_indices_map.filter(t => {
-            t._1 < d._1 && t._2 > d._2
-          })
-          println("filtered: " + filtered.mkString(" "))
-          filtered.size
-        }
-        println(e.sum)
-      }
-    }
-
   }
 
   def main(args: Array[String]): Unit = {
