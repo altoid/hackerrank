04a80abea4dbc5bbb2d239200b0b274904ecf07f
Doug Tomm <dtomm@radiumone.com>
2019-04-04 00:51:59 -0700

    works but blows stack

--- max_array_sum/s.py
+++ max_array_sum/s.py
@@ -21,29 +21,76 @@ def subsets(arr):
     for s in subsets(arr[1:]):
         yield s
 
-class MyTest(unittest.TestCase):
+def solution(arr):
+    max_sum = None
+
+    for s in subsets(arr):
+        current_sum = sum(s)
+        if max_sum is None:
+            max_sum = current_sum
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+
+    return max_sum
+
+class SumTest(unittest.TestCase):
+    def test1(self):
+        arr = [3,5,-7,8,10]
+        self.assertEqual(15, solution(arr))
+
+    def test2(self):
+        arr = [3,7,4,6,5]
+        self.assertEqual(13, solution(arr))
+
+    def test3(self):
+        with open('input00.txt') as f:
+            f.readline()  # throw it away
+            arr = map(int, f.readline().strip().split(' '))
+        print arr[0]
+        print len(arr)
+        self.assertEqual(151598486, solution(arr))
+
+class SubsetTest(unittest.TestCase):
 
     def test1(self):
+        print
         arr = [1]
         for s in subsets(arr):
             print s
 
     def test2(self):
+        print
         arr = [1,2]
         for s in subsets(arr):
             print s
 
     def test3(self):
+        print
         arr = [1,2, 3]
         for s in subsets(arr):
             print s
 
     def test4(self):
+        print
         arr = [1,2, 3, 4]
         for s in subsets(arr):
             print s
 
     def test5(self):
+        print
         arr = [1,2, 3, 4, 5]
+        for s in subsets(arr):
+            print s
+
+    def test6(self):
+        print
+        arr = [1,2, 3, 4, 5, 6]
+        for s in subsets(arr):
+            print s
+
+    def test7(self):
+        print
+        arr = [1,2, 3, 4, 5, 6, 7]
         for s in subsets(arr):
             print s
