143caf9a60ecccdbfc6a865aca7dc8042fea5193
Doug Tomm <dtomm@radiumone.com>
2019-04-18 00:44:22 -0700

    checkpoint

--- challenges/q.sql
+++ challenges/q.sql
@@ -8,7 +8,7 @@ group by hacker_id
 order by count(*), hacker_id
 ;
 
-
+/*
 select c, count(*) cc
 from
 (
@@ -29,9 +29,12 @@ from
     order by count(*), hacker_id
 ) y
 ;
+*/
 
 select
-*
+a.c,
+a.cc,
+b.maxcount
 from
 (
     select c, count(*) cc
@@ -82,3 +85,48 @@ inner join
 on a.c = b.c
 ;
 */
+
+select
+a.*,
+b.*,
+hackers.name
+from
+(
+    select hacker_id, count(*) c
+    from challenges
+    group by hacker_id
+    order by count(*), hacker_id
+) a
+inner join
+(
+    select
+    a.c,
+    a.cc,
+    b.maxcount
+    from
+    (
+        select c, count(*) cc
+        from
+        (
+            select count(*) c
+            from challenges
+            group by hacker_id
+        )x
+        group by c
+    ) a
+    inner join
+    (
+        select max(c) maxcount
+        from
+        (
+            select hacker_id, count(*) c
+            from challenges
+            group by hacker_id
+        ) y
+    ) b
+    where  cc < 2 or c = maxcount
+) b
+on a.c = b.c
+inner join hackers on hackers.hacker_id = a.hacker_id
+order by hacker_id
+;
