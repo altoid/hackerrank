c738ae766e892cdf22eb72dfc2cd7e31b38ae278
Douglas Tomm <douglas.tomm@amobee.com>
2021-03-07 02:48:25 -0800

    rewrote left/right, all tests pass

--- max_index_product/solution.py
+++ max_index_product/solution.py
@@ -3,63 +3,63 @@
 import fileinput
 import unittest
 
-# TODO:  add 1 to each element in left, right when lefts, rights, are finished.
 
 class Solution(object):
     def __init__(self, arr):
         self.left = [0] * len(arr)
         self.right = [0] * len(arr)
         self.arr = arr
+        self.maxproduct = 0
+        self.products = []
         if arr:
             self.lefts()
             self.rights()
+
+            self.products = [a * b for a, b in zip(self.left, self.right)]
+            self.maxproduct = max(self.products)
 
     def lefts(self):
         """
         compute left(i) for every element in arr
         left[0] == left[-len] == 0
         """
+        lstack = [len(self.arr)]
         i = len(self.arr) - 1
-        halt = -1
-        while True:
-            right = i
-            interval = [right, right]
+        while i >= 0:
+            if self.arr[i] <= self.arr[lstack[-1] - 1]:
+                lstack.append(i + 1)
+            else:
+                while lstack and self.arr[i] > self.arr[lstack[-1] - 1]:
+                    x = lstack.pop()
+                    self.left[x - 1] = i + 1
+                lstack.append(i + 1)
             i -= 1
-            while i > halt:
-                if self.arr[i] > self.arr[right]:
-                    break
-                interval[0] = i
-                i -= 1
-            if i == halt:
-                break
 
-            # self.arr[i] > self.arr[right]
-            interval[0] = i
-            for j in xrange(interval[1], interval[0], -1):
-                self.left[j] = interval[0] + 1
-    
     def rights(self):
         """
         compute right(i) for every element in arr
         right[len - 1] = 0
-        """
-        i = 0
-        while True:
-            left = i
-            interval = [left, left]
-            i += 1
-            while i < len(self.arr):
-                if self.arr[i] > self.arr[left]:
-                    break
-                interval[1] = i
-                i += 1
-            if i == len(self.arr):
-                break
 
-            # self.arr[i] > self.arr[left]
-            interval[1] = i
-            for j in xrange(interval[0], interval[1]):
-                self.right[j] = interval[1] + 1
+        create an empty stack
+        put first element on it
+        for each successive element
+            if it is smaller than top of stack, put it on
+            else pull everything off the stack that is smaller
+            their rights are the index of the current element
+            put current element on stack
+        anything left on the stack has a right of 0
+        """
+        rstack = [1]
+        i = 1
+        while i < len(self.arr):
+            if self.arr[i] <= self.arr[rstack[-1] - 1]:
+                rstack.append(i + 1)
+            else:
+                while rstack and self.arr[i] > self.arr[rstack[-1] - 1]:
+                    x = rstack.pop()
+                    self.right[x - 1] = i + 1
+                rstack.append(i + 1)
+            i += 1
 
 
 def solve(arr):
@@ -148,3 +148,22 @@ class MyTest(unittest.TestCase):
         sol = Solution(arr)
         self.assertEqual([0, 1, 1, 3, 3, 5, 5, 7], sol.left)
 
+    ###
+
+    def test_both_5(self):
+        arr = [6, 2, 5, 3, 4, 1, 7]
+        sol = Solution(arr)
+        self.assertEqual([7, 3, 7, 5, 7, 7, 0], sol.right)
+        self.assertEqual([0, 1, 1, 3, 3, 5, 0], sol.left)
+
+    def test_both_6(self):
+        arr = [6, 2, 5, 4, 5, 1, 6]
+        sol = Solution(arr)
+        self.assertEqual([0, 3, 7, 5, 7, 7, 0], sol.right)
+        self.assertEqual([0, 1, 1, 3, 1, 5, 0], sol.left)
+
+    def test_example(self):
+        arr = [5, 4, 3, 4, 5]
+        sol = Solution(arr)
+        self.assertEqual([0, 5, 8, 5, 0], sol.products)
+        self.assertEqual(8, sol.maxproduct)
