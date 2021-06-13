6e203629825c77aad15096d0fcfa4195ecb7e954
Doug Tomm <dctomm@gmail.com>
2021-06-13 07:34:23 -0700

    checkpoint

--- two_sigma/friends.py
+++ two_sigma/friends.py
@@ -16,9 +16,25 @@ def friendCircles(friends):
     nfriends = len(friends)
 
     for i in xrange(nfriends):
-        for j in xrange(i, nfriends):
-            print j,
-        print
+        if friends[i][i] == 'Y':
+            circles += 1
+            wash(i, friends)
+
+    return circles
+
+
+def wash(i, friends):
+    nfriends = len(friends)
+    if friends[i][i] == 'Y':
+        friends[i][i] = '-'
+    for j in xrange(i + 1, nfriends):
+        if friends[i][j] == 'Y':
+            friends[i][j] = '-'
+            wash(j, friends)
+            for k in xrange(i, j + 1):
+                if friends[k][j] == 'Y':
+                    friends[k][j] = '-'
+                    wash(k, friends)
 
 
 if __name__ == '__main__':
@@ -40,5 +56,12 @@ if __name__ == '__main__':
 class MyTest(unittest.TestCase):
     def test1(self):
         arr = [['Y', 'Y', 'N', 'N'], ['Y', 'Y', 'Y', 'N'], ['N', 'Y', 'Y', 'N'], ['N', 'N', 'N', 'Y']]
+
+        print arr[0][3]
+
+        self.assertEqual(2, friendCircles(arr))
+
+    def test2(self):
+        arr = [['Y', 'N', 'Y', 'N'], ['N', 'Y', 'Y', 'N'], ['Y', 'Y', 'N', 'N'], ['N', 'N', 'N', 'Y']]
 
         self.assertEqual(2, friendCircles(arr))
